069b7333660845330a490694a97e5d16a5fefdbd
Sam Mingo <github@lamolabs.org>
2021-05-17 00:29:09 -0400

    Makefile commit

--- model_cmds/01_vpn.sh
+++ model_cmds/01_vpn.sh
@@ -26,20 +26,40 @@ pasteCmd="gpaste"
 
 
 ##### ON VPN
+### $ scutil --nwi
+### Network information
 ###
+### IPv4 network interface information
+###      en0 : flags      : 0x5 (IPv4,DNS)
+###            address    : 192.168.13.10
+###            reach      : 0x00000002 (Reachable)
+###    utun2 : flags      : 0x7 (IPv4,IPv6,DNS)
+###            address    : 172.31.250.118
+###            reach      : 0x00000002 (Reachable)
+###
+###    REACH : flags 0x00000002 (Reachable)
+###
+### IPv6 network interface information
+###    utun2 : flags      : 0x7 (IPv4,IPv6,DNS)
+###            address    : 2001:db8::b
+###            reach      : 0x00000002 (Reachable)
+###
+###    REACH : flags 0x00000002 (Reachable)
+###
+### Network interfaces: en0 utun2
 
 #----------------------------------------------------
 
 
 
- dnsResolverChk() {
+dnsResolverChk() {
 
-     verbose=0; [[ $1 -eq 1 ]] && local verbose=1
-     [[ $verbose -eq 1 ]] && set -x
+ verbose=0; [[ $1 -eq 1 ]] && local verbose=1
+ [[ $verbose -eq 1 ]] && set -x
 
-     [[ $verbose -eq 1 ]] && set +x
-     echo ''
-     echo ''
- }
+ [[ $verbose -eq 1 ]] && set +x
+ echo ''
+ echo ''
+}
 
- #----------------------------------------------------
+#----------------------------------------------------

ae67075d905c38200467a0c59a616df4fc9d5251
Sam Mingo <github@lamolabs.org>
2021-05-17 21:59:39 -0400

    Makefile commit

--- cmd/svcs.go
+++ cmd/svcs.go
@@ -32,13 +32,8 @@ import (
 // svcsCmd represents the svcs command
 var svcsCmd = &cobra.Command{
 	Use:   "svcs",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "TBD",
+	Long:  `TBD`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("svcs called")
 	},
